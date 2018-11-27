0b74aa62fcc9efd7765fe98eed6a094f575c52cf
alexz <test@email.ru>
2018-11-28 00:01:03 +0700

    с помощью метода fromJson() преобразовал JSON в объект Java

--- src/main/java/example2/GsonExample.java
+++ src/main/java/example2/GsonExample.java
@@ -6,9 +6,8 @@
 package example2;
 
 import com.google.gson.Gson;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+
+import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,6 +20,9 @@ public class GsonExample {
     DataObject object1 = new DataObject();
     Gson gson = new Gson();
     String jsonString = gson.toJson(object1);
+    System.out.println("jsonString = " + jsonString);
+
+    System.out.println("Запишем строку jsonString в файл");
     try (FileWriter fileWriter = new FileWriter(FILENAME)) {
       fileWriter.write(jsonString);
     } catch (IOException e) {
@@ -28,5 +30,20 @@ public class GsonExample {
     }
     // В результате создан файл file.json с содержимым
     // {"data1":"Some value","data2":123,"list":["List item 1","List item 2","List item 3"]}
+
+    // ------------------------------------------------------------------------
+    // Теперь для преобразования полученной на предыдущем шаге JSON-строки
+    // обратно в Java-объект воспользуемся методом fromJson()
+    // ------------------------------------------------------------------------
+    System.out.println("\nПреобразуем JSON-строку в Java-объект");
+    try {
+      BufferedReader reader = new BufferedReader(new FileReader(FILENAME));
+      DataObject object2 = gson.fromJson(reader, DataObject.class);
+      System.out.println("object2 = " + object2);
+    } catch (FileNotFoundException e) {
+      Logger.getLogger(GsonExample.class.getName()).log(Level.SEVERE, null, e);
+    }
+    // В результате получаем Java-объект
+    // [DataObject: data1=Some value, data2=123, list=[List item 1, List item 2, List item 3]]
   }
 }
