f7a50085163cf62cd998578a61bd5284c7f7fc1b
alexz <test@email.ru>
2018-11-28 02:07:52 +0700

    получаю Java-объект из XML-файла

--- src/main/java/example3/JaxbExample.java
+++ src/main/java/example3/JaxbExample.java
@@ -3,6 +3,7 @@ package example3;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -10,6 +11,8 @@ import java.util.logging.Logger;
 public class JaxbExample {
 
   public static void main(String[] args) {
+    System.out.println("\n***** Marshalling: Преобразование Java-объекта в XML-файл *****\n");
+
     // Создаём объект-Java
     Customer customer1 = new Customer();
     customer1.setId(1);
@@ -29,11 +32,33 @@ public class JaxbExample {
     } catch (JAXBException e) {
       Logger.getLogger(JaxbExample.class.getName()).log(Level.SEVERE, null, e);
     }
+
+    System.out.println("\nXML-файл записан");
     // В результате получаем xml-файл:
     // <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
     // <customer id="1">
     //   <age>22</age>
     //   <name>Вася Пупкин</name>
     // </customer>
+
+    // --------------------------------------------------------------------------
+
+    System.out.println("\n***** Unmarshalling: Преобразование XML-файла в Java-объект *****\n");
+
+    // Выполним обратную операцию: преобразуем XML-файл в Java объект.
+    // Это делается с помощью метода Unmarshaller.unmarshal()
+    // В классе Unmarshaller есть много перегруженных методов unmarshal()
+    // которые решают большинство распространенных случаев использования.
+    try {
+      File fileXml = new File("testfiles\\customer.xml");
+      JAXBContext context = JAXBContext.newInstance(Customer.class);
+      Unmarshaller unmarshaller = context.createUnmarshaller();
+      Customer customer2 = (Customer) unmarshaller.unmarshal(fileXml);
+      System.out.println("customer2 = " + customer2);
+    } catch (JAXBException e) {
+      Logger.getLogger(JaxbExample.class.getName()).log(Level.SEVERE, null, e);
+    }
+    // В результате получаем Java-объект
+    // Customer [name=Вася Пупкин, id=1, age=22]
   }
 }
