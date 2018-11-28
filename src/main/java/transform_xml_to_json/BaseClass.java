726d02bf80fe7e22a7c65a168f95593b41072df8
alexz <test@email.ru>
2018-11-28 22:36:55 +0700

    создал метод fromXmlToObject( )

--- src/main/java/transform_xml_to_json/BaseClass.java
+++ src/main/java/transform_xml_to_json/BaseClass.java
@@ -3,6 +3,7 @@ package transform_xml_to_json;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -12,9 +13,8 @@ class BaseClass {
   // -------------------------------------------
   // Преобразование Java-объекта в XML-файл
   // -------------------------------------------
-  protected static void fromObjectToXml(Company company) {
+  protected static void fromObjectToXml(Company company, File fileXml) {
     try {
-      File fileXml = new File("testfiles\\company.xml");
       JAXBContext context = JAXBContext.newInstance(Company.class);
       Marshaller marshaller = context.createMarshaller();
       marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
@@ -23,5 +23,22 @@ class BaseClass {
     } catch (JAXBException e) {
       Logger.getLogger(DemoXmlToJson.class.getName()).log(Level.SEVERE, null, e);
     }
+  }
+
+  // -------------------------------------------
+  // Преобразование XML-файла в Java-объект
+  // -------------------------------------------
+  protected static Company fromXmlToObject(File fileXml) throws JAXBException {
+    JAXBContext context = JAXBContext.newInstance(Company.class);
+    Unmarshaller unmarshaller = context.createUnmarshaller();
+    Company company = (Company) unmarshaller.unmarshal(fileXml);
+    return company;
+  }
+
+  // -------------------------------------------
+  // Преобразование Java-объекта в JSON-файл
+  // -------------------------------------------
+  protected static void fromObjectToJson(Company company, File fileJson) {
+    //
   }
 }
--- src/main/java/transform_xml_to_json/DemoXmlToJson.java
+++ src/main/java/transform_xml_to_json/DemoXmlToJson.java
@@ -6,61 +6,56 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 class DemoXmlToJson extends BaseClass {
 
+  // Для записи JSON-файла
+  private static File companyJson = new File("testfiles\\company.json");
+  private static final String absolutePathToFile = companyJson.getAbsolutePath(); // абсолютный путь к файлу начиная с корня системы
+  private static final String FILENAME = absolutePathToFile;
+
   public static void main(String[] args) {
-    System.out.println("\n***** Marshalling: Преобразование Java-объекта в XML-файл *****\n");
-
-    Company bcsFromXml;
-
-    // Создаём объект-Java
+    System.out.println("Создаём объект-Java");
     Company bcs = new Company();
     bcs.setId(601);
     bcs.setName("БКС");
     bcs.setEmail("bcs@testemail.ru");
     bcs.setInn(112901);
 
+    File fileXml = new File("testfiles\\company.xml");
+
     System.out.println("Преобразование Java-объекта в XML-файл");
-    fromObjectToXml(bcs);
+    fromObjectToXml(bcs, fileXml);
 
-    /* try {
-      File fileXml = new File("testfiles\\company.xml");
-      JAXBContext context = JAXBContext.newInstance(Company.class);
-      Marshaller marshaller = context.createMarshaller();
-      marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-      marshaller.marshal(bcs, fileXml);
-      marshaller.marshal(bcs, System.out); // вывод на консоль
-    } catch (JAXBException e) {
-      Logger.getLogger(DemoXmlToJson.class.getName()).log(Level.SEVERE, null, e);
-    } */
-
-    // В результате получаем xml-файл
-
-    // -------------------------------------------------------------------------------
-
-    System.out.println("\n***** Unmarshalling: Преобразование XML-файла в Java-объект *****\n");
-
+    System.out.println("Преобразование XML-файла в Java-объект");
     try {
-      File fileXml = new File("testfiles\\company.xml");
-      JAXBContext context = JAXBContext.newInstance(Company.class);
-      Unmarshaller unmarshaller = context.createUnmarshaller();
-      bcsFromXml = (Company) unmarshaller.unmarshal(fileXml);
+      Company bcsFromXml = fromXmlToObject(fileXml);
       System.out.println("bcsFromXml = ");
       System.out.println(bcsFromXml);
+
+      // Преобразование Java-объекта в JSON-файл
+      System.out.println("Преобразование Java-объекта в JSON-файл");
+      Gson gson = new Gson();
+      String bcsToJsonString = gson.toJson(bcsFromXml);
+      System.out.println("bcsToJsonString = " + bcsToJsonString);
+      System.out.println("Запишем строку bcsToJsonString в файл");
+      try (FileWriter fileWriter = new FileWriter(FILENAME)) {
+        fileWriter.write(bcsToJsonString);
+      } catch (IOException e) {
+        Logger.getLogger(DemoXmlToJson.class.getName()).log(Level.SEVERE, null, e);
+      }
+
     } catch (JAXBException e) {
       Logger.getLogger(DemoXmlToJson.class.getName()).log(Level.SEVERE, null, e);
     }
-    // В результате получаем Java-объект
 
-    // -------------------------------------------------------------------------------
-
-    System.out.println("\n***** Преобразование Java-объекта в JSON-файл *****\n");
+    // System.out.println("Преобразование Java-объекта в JSON-файл");
+    // fromObjectToJson();
     // Gson gson = new Gson();
     // String bcsToJsonString = gson.toJson(bcsFromXml);
-
-
   }
 }
